3d2c62a55436a2d6d399978d14e2f175f26a08d4
Jesus Velasquez <jesusvelasquezoficial@gmail.com>
2018-08-17 01:53:10 -0400

    Registro e inicio de session

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,5 +1,22 @@
 module.exports = (app, passport) => {
+
   app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index',{
+      message: req.flash('loginMessage')
+    });
   });
+
+  app.post('/signin', (req, res) => {
+
+  });
+
+  app.get('/signup', (req, res) => {
+    res.render('signup',{
+      message: req.flash('signupMessage')
+    });
+
+  app.post('/signup', (req, res) => {
+
+  });
+
 };
